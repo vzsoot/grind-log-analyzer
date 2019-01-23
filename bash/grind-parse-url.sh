48ac715af09807f3311f2d28a742e865fcaa439c
Zsolt Venczel <vzsoot@gmail.com>
2019-01-23 11:36:19 +0100

    script location reference fix

--- bash/grind-parse-url.sh
+++ bash/grind-parse-url.sh
@@ -4,4 +4,6 @@ GRIND_DB_URL=${GRIND_DB_URL:=jdbc:postgresql://localhost:5432/grind-logs}
 GRIND_DB_USER=${GRIND_DB_USER:=postgres}
 GRIND_DB_PASS=${GRIND_DB_PASS:=123}
 
-java -jar ../release/grind-log-analyzer.jar --dburl=$GRIND_DB_URL --dbuser=$GRIND_DB_USER --dbpass=$GRIND_DB_PASS --grindUrl=$1
+parent_path=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
+
+java -jar $parenth_path/../release/grind-log-analyzer.jar --dburl=$GRIND_DB_URL --dbuser=$GRIND_DB_USER --dbpass=$GRIND_DB_PASS --grindUrl=$1
